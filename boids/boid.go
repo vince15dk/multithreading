a007da14102d1a6b6a09c0d5bb167bbc08039467
Suk Joo Kim <sj@Suks-MacBook-Pro.local>
2021-01-25 21:14:14 +0900

    multithreading

--- boids/boid.go
+++ boids/boid.go
@@ -1,7 +1,33 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
 type Boid struct {
 	position Vector2D
 	velocity Vector2D
 	id       int
+}
+
+func (b *Boid) moveOne() {
+	b.position = b.position.Add((b.velocity))
+}
+
+func (b *Boid) start() {
+	for {
+		b.moveOne()
+		time.Sleep(5 * time.Microsecond)
+	}
+}
+
+func createBoid(bid int) {
+	b := Boid{
+		position: Vector2D{rand.Float64() * screenWidth, rand.Float64() * screenHeight},
+		velocity: Vector2D{(rand.Float64() * 2) - 1.0, (rand.Float64() * 2) - 1.0},
+		id:       bid,
+	}
+	boids[bid] = &b
+	go b.start()
 }
---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/vince15dk/multithreading
+
+go 1.15
+require (
+	github.com/hajimehoshi/ebiten/v2 v2.0.2
+)
